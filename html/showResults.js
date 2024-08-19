3e3ec4dd15daa261e2d9122bcd91a64e7f7b5bce
AmanDevelops <amanpal2072004@gmail.com>
2024-08-19 19:38:30 +0530

    1.1

--- html/showResults.js
+++ html/showResults.js
@@ -1,28 +1,44 @@
 
 const firebaseConfig = {
-    projectId: "uptac-2ae80",
+    projectId: "uptac-2024",
 };
 
 
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
+function CalculateProbability(rank, or,cr){
+    if (rank/or < 0.8){
+        return '<p style="color: green; ">Very High Probability</p>';
+    }
+    else if (rank < or){
+        return '<p style="color: green;">High Probability</p>';
+    }
+    else if (rank > or && rank < cr){
+        return '<p style="color: yellow; text-shadow: color: white; -webkit-text-stroke-width: 0.40px; -webkit-text-stroke-color: red;">Moderate Probability</p>';
+    }
+    else if (rank/cr < 1.1) {
+        return '<p style="color: red; ">Low Probability</p>';
+    }
+    else if (rank > cr) {
+        return '<p style="color: red;">Very Low Probability</p>';
+    }
+    
+}
 
-async function filterData(category, quota, course, crl) {
+async function filterData(round,category, quota, course, crl) {
     try {
-        let queryRef = db.collection('round1')
+        let queryRef = db.collection('btech')
+            .where('round', '==', round)
             .where('category', '==', category)
             .where('quota', '==', quota)
             .where('cr', '>', Number(crl));
 
         if (course !== "allprograms") {
-            queryRef = queryRef.where('program', '==', course);
+            queryRef = queryRef.where('branch', '==', course);
         }
 
         const querySnapshot = await queryRef.get();
-
-
-
 
         const tableBody = document.getElementById('tableDataBody');
         tableBody.innerHTML = '';
@@ -33,16 +49,19 @@ async function filterData(category, quota, course, crl) {
             const data = doc.data();
             tableRows.push(`
           <tr>
-            <td>${data.college}</td>
-            <td>${data.program}</td>
+            <td>${data.name}</td>
+            <td>${data.branch}</td>
             <td>${data.category}</td>
+            <td>${data.or}</td>
             <td>${data.cr}</td>
+            <td>${CalculateProbability(Number(crl), Number(data.or), Number(data.cr))}</td>
           </tr>
         `);
         });
 
         // Update the table body with all rows at once for better performance
         tableBody.innerHTML = tableRows.join('');
+        document.getElementById('resultsheader').innerText = "Showing Results for Round "+ round;
 
     } catch (error) {
         console.error("Error filtering data: ", error);
@@ -56,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     if (formData) {
         const responseDiv = document.getElementById('response');
         event.preventDefault();
-        filterData(formData['category'], formData['quota'], formData['course'], formData['crl']);
+        filterData(formData['round'],formData['category'], formData['quota'], formData['course'], formData['crl']);
     } else {
         alert('No Colleges Found!');
     }
---
+++ python/utils.py
@@ -0,0 +1,50 @@
+import csv, firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+def get_select_values(filename, position, outputfile="select_values.html"):
+    """Create the list of all the branches or unique values of a column and convert to html select value"""
+    # 1. Reading Data From Source File
+    source_file = open(filename, 'r')
+    data = csv.reader(source_file)
+
+    # 2. separating unique values to string
+    unique_values = list()
+    for i in data:
+        if i[position] not in unique_values:
+            unique_values.append(i[position])
+
+    # 3. formatting the final list
+    final_list = list()
+    for i in unique_values:
+        final_list.append(f"<option value='{i}'>{i}</option>\n")
+
+    # 4. Writing Output Data
+    with open(outputfile, 'w') as f2:
+        f2.writelines(final_list)
+
+def push_data_to_firebase(secret_file, filename):
+    """Pushes data from csv to firebase firestore"""
+    cred = credentials.Certificate(secret_file)
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    collection_ref = db.collection('btech')
+
+    f= open(filename, 'r')
+    data = csv.reader(f)
+
+    total = len(data)
+    n = 1
+
+    for i in data:
+        doc_ref = collection_ref.add({
+            'round': '1',
+            'name': i[2],
+            'branch': i[3],
+            'quota': i[5],
+            'category': i[6],
+            'or': int(float(i[8])),
+            'cr': int(float(i[9]))
+        })
+        print(f"[{n}/{total}] Succesfully Added '{i[2]}' to Database!")
+        n+=1
